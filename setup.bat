3ee29f545d5439fbcfb9fdda92683a4042579940
Justin Brown <jbrown@bpoc.org>
2016-12-28 10:47:08 -0800

    Better setup.bat

--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
 powershell.exe "Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force"
 powershell.exe "Invoke-RestMethod -URI https://raw.githubusercontent.com/behindcurtain3/powershell-scripts/master/setup-workstation.ps1 -Method Get -OutFile $env:USERPROFILE\setup-workstation.ps1"
-powerhsell.exe "Unblock-File $env:USERPROFILE\setup-workstation.ps1"
-powershell.exe $env:USERPROFILE\setup-workstation.ps1
+powershell.exe "Unblock-File $env:USERPROFILE\setup-workstation.ps1"
+powershell.exe "%USERPROFILE%\setup-workstation.ps1"
 del "%USERPROFILE%\setup-workstation.ps1"
 powershell.exe "Set-ExecutionPolicy -ExecutionPolicy Default -Force"
